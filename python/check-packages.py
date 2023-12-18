45c4021ac66d78fc471aea44828e9b0cb6a9a45a
bhincean <147591316+bhincean@users.noreply.github.com>
2023-12-18 17:05:03 +0100

    Update check-packages.py

--- python/check-packages.py
+++ python/check-packages.py
@@ -6,24 +6,28 @@ malware=[]
 
 ##data=read_csv("pkgs.csv", delimiter=',')
 
-data_csv = csv.reader("pkgs.csv")
-for row in data_csv:
-    malware.append(row)
+with open('mal.json', mode='r') as f1:
+    malware_dict=json.load(f1)
 
-with open('output.json', mode='r') as f:
-    data_json=json.load(f)
+##for (i, j) in malware_dict:
+##    malware.append(['name'])
 
-for j in data_json:
-    packages.append(j['name'])
+with open('output.json', mode='r') as f2:
+    packages_dict=json.load(f2)
+
+##for j in packages_dict:
+##    packages.append(j['name'])
+
 
 deinstall=[]
 
-for item1 in packages:
-    for item2 in malware:
-        if item1 == item2:
-            deinstall.append(item1)
+for item1 in packages_dict:
+    for item2 in malware_dict:
+        if item1['name'] == item2['name']:
+            deinstall.append(item2)
         else:
             continue
+
 if len(deinstall) == 0:
     print("No malicious packages were found") 
 else:
